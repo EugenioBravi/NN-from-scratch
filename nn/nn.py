0f79ee7382bd9e905cf5b1bd37fe27e277b8cd6a
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-21 15:27:15 -0300

    Can decide if neurons have or not bias

--- nn/nn.py
+++ nn/nn.py
@@ -4,13 +4,16 @@ from typing import Type
 
 class Neuron():
 
-    def __init__(self,n_weights: int,bias : float = round(random.uniform(-1,1),2) ) -> None:
+    def __init__(self,n_weights: int,bias : bool = False ) -> None:
 
         if type(n_weights) != int or n_weights <= 0 or n_weights >20 :
             raise Exception('The param n_weights should be an int beetwen 1 and 20')
 
         self.weights = [round(random.uniform(-1,1),2) for _ in range(n_weights)] 
-        self.bias = bias
+        if bias == True:
+            self.bias = round(random.uniform(-1,1),2)
+        else:
+            self.bias = 0
 
 class Layer():
     def __init__(self,n_weights:int,n_neurons:int) -> None:
