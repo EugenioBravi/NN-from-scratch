a31b4821a98da5a13c347b805f21156b342a5cc0
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-21 14:59:45 -0300

    Adds utils file with a spiral dataset generator and a data spliter

---
+++ utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+def spiral_dataset(samples,classes):
+    random.seed(0)
+    N = samples # number of points per class
+    D = 2 # dimensionality
+    K = classes # number of classes
+    X = np.zeros((N*K,D)) # data matrix (each row = single example)
+    y = np.zeros(N*K, dtype='uint8') # class labels
+    for j in range(K):
+        ix = range(N*j,N*(j+1))
+        r = np.linspace(0.0,1,N) # radius
+        t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+        X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+        y[ix] = j
+    return X,y
+
+def data_split(X,y,p):
+    #splits the data in a percentage p (0 > p < 1)
+    if p <= 0 or p >=1:
+        raise Exception('p value should be between 0 and 1') 
+    
+    split = len(X)* p
+    X_train = X[:split]
+    X_test = X[split:]
+    y_train = y[:split]
+    y_test = y[split:]
+    return X_train, X_test, y_train, y_test
