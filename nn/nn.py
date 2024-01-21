0b12db32c24db0c4272d207ab4960ea04ae8e49e
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-21 13:53:53 -0300

    Moves neuron, layer, network file into a single file to avoid circular dependency

--- nn/nn.py
+++ nn/nn.py
@@ -1,5 +1,17 @@
+import random
 import numpy as np
-from neuron import Neuron
+from typing import Type
+
+class Neuron():
+
+    def __init__(self,n_weights: int,bias : float = round(random.uniform(-1,1),2) ) -> None:
+
+        if type(n_weights) != int or n_weights <= 0 or n_weights >20 :
+            raise Exception('The param n_weights should be an int beetwen 1 and 20')
+
+        self.weights = [round(random.uniform(-1,1),2) for _ in range(n_weights)] 
+        self.bias = bias
+
 class Layer():
     def __init__(self,n_weights:int,n_neurons:int) -> None:
         
@@ -38,3 +50,22 @@ class Layer():
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0,keepdims=True)
         self.dinputs = np.dot(dvalues, self.get_weights().T)
+
+class Network():
+    def __init__(self) -> None:
+        self.layers = []
+        
+    #Allows adding one layer at a time at the end of the sequence of layers OR to replace the whole network of layers 
+    def add_layer(self,layer:Type[Layer] or [Type[Layer]])->None:
+        if type(layer) == list:
+            self.layers = layer
+        else:
+            self.layers.append(layer)
+
+    def forward(self,input:[float]) -> [float]:
+        if type(input) != list:
+            raise Exception('The input should be a list')
+        layers_input = input
+        for layer in self.layers:
+            layers_input = layer.forward(layers_input)
+        return layers_input
