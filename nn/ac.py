568cdbea83176b6122cfa68d54b9b340917ceddd
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-21 22:01:37 -0300

    Adds the softmax activation function

--- nn/ac.py
+++ nn/ac.py
@@ -6,3 +6,16 @@ class ReLU():
     def forward(self,inputs):
         self.output = np.maximum(0,inputs)
         return self.output
+
+class softmax():
+    def forward(self,inputs):
+        #Get unnormalized probabilities
+
+        #" inputs - np.max(inputs) " to prevent the exponential function from overflowing, with Softmax, thanks to the normalization, we can subtract any value from all of the inputs, and it will not change the output
+
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+         # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,keepdims=True)
+        
+        self.output = probabilities
+        return self.output
