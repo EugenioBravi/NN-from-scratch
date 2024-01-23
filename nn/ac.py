4e0ecd343c3a1f8b738a8167543767d1bdfebbb5
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-23 16:29:29 -0300

    Implements softmax backpropagation

--- nn/ac.py
+++ nn/ac.py
@@ -23,3 +23,16 @@ class softmax():
         
         self.output = probabilities
         return self.output
+    
+    def backward(self, dvalues):
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
