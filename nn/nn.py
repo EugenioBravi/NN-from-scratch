5d24e47551a59c24abe56e2a514ec58cc33f20ff
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-23 20:12:36 -0300

    Fix typo

--- nn/nn.py
+++ nn/nn.py
@@ -54,7 +54,7 @@ class Layer():
     def backward(self,dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0,keepdims=True)
-        self.dinputs = np.dot(dvalues, self.get_weights().T)
+        self.dinputs = np.dot(dvalues, self.weights)
         return self.dinputs
 
 class Network():
