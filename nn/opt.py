b4bf3a262ffd38dd59df37e6d2f2f6291386d356
EugenioBravi <98536791+EugenioBravi@users.noreply.github.com>
2024-01-24 11:12:14 -0300

    Implemente learning decay in SGD fn

--- nn/opt.py
+++ nn/opt.py
@@ -2,12 +2,23 @@ import numpy as np
 
 class Optimizer_SGD():
 
-    def __init__(self, network, learning_rate=0.3):
-        self.learnig_rate = learning_rate
+    def __init__(self, network, learning_rate = 1., decay = 0.):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
         self.network = network
+        self.decay = decay
+        self.iterations = 0
+
+    def learning_rate_decay(self):
+        self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
 
     def step(self):
+
         for layer in self.network.layers:
             if hasattr(layer, 'weights'):    
-                layer.weights += -self.learnig_rate * layer.dweights.T
-                layer.biases += -self.learnig_rate * layer.dbiases[0]
+                layer.weights += -self.current_learning_rate * layer.dweights.T
+                layer.biases += -self.current_learning_rate * layer.dbiases[0]
+        
+        if self.decay:
+            self.learning_rate_decay()
+            self.iterations += 1
